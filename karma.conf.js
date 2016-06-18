7e142f8a0a1291337c68e27dd71d25e23fe61e84
Gabriel Perales <gabrielperales@gmail.com>
2016-06-18 13:56:38 +0100

    changed to test with PhantomJS instead of Chrome

--- karma.conf.js
+++ karma.conf.js
@@ -1,10 +1,14 @@
 module.exports = function(config) {
   config.set({
     frameworks: ['jasmine'],
+    plugins : ['karma-jasmine', 'karma-phantomjs-launcher'],
 
     files: [
       'lib/spyon.js',
       'test/*.js',
-    ]
-  })
+    ],
+
+    // web server port
+    port: 9876
+  });
 }
